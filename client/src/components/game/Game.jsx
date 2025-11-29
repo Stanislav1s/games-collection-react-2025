bc6836201b5f8d5e5b38674cfa882a3d24395657
Stanislav1s <stanislavstanislavov@Stanislavs-MacBook-Air.local>
2025-11-29 13:12:32 +0200

    Add dynamic catalog

---
+++ client/src/components/game/Game.jsx
@@ -0,0 +1,24 @@
+export default function Game({
+    _id,
+    title,
+    genre,
+    players,
+    date,
+    imageUrl,
+    summary,
+    _createdOn,
+}) {
+    return (
+        <div className="catalog-container">
+            <div className="game">
+                <img src={imageUrl} alt={title} />
+                <div className="details-overlay">
+                    <p className="name">{title}</p>
+                    <p className="genre">{genre}</p>
+                    <a href="#" className="details-button">
+                        {summary}
+                    </a>
+                </div>
+            </div>
+        </div>)
+}
