8136ccee4ef840e8fbe9d86d5120a4871b5b7d93
[Stanislav Stanislavov] <slavivd95@abv.bg>
2026-07-07 23:23:18 +0300

    update detailsComments

--- client/src/components/details/details-comments/DetailsComments.jsx
+++ client/src/components/details/details-comments/DetailsComments.jsx
@@ -1,22 +1,30 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import useRequest from "../../../hooks/useRequest.js";
+
 export default function DetailsComments() {
+  const { request } = useRequest();
+  const { gameId } = useParams();
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    request("/comments").then((result) => {
+      const gameComments = result.filter(
+        (comment) => comment.gameId === gameId
+      );
+      setComments(gameComments);
+    });
+  }, [gameId]);
   return (
     <div className="details-comments">
       <h2>Comments:</h2>
       <ul>
-        <li className="comment">
-          <p>
-            Content: A masterpiece of world design, though the boss fights are
-            brutal.
-          </p>
-        </li>
-        <li className="comment">
-          <p>
-            Content: Truly feels like a next-gen evolution of the Souls formula!
-          </p>
-        </li>
+        {comments.map((comment) => (
+          <li key={comment._id} className="comment">
+            <p>email: {comment.message}</p>
+          </li>
+        ))}
       </ul>
-      {/* Display paragraph: If there are no games in the database */}
-      {/* <p class="no-comment">No comments.</p> */}
+      {comments.length === 0 && <p className="no-comment">No comments.</p>}
     </div>
   );
 }
