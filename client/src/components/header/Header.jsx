e91bf35ba88919c3df22ce5a0827cc08f8780916
[Stanislav Stanislavov] <slavivd95@abv.bg>
2026-07-16 11:29:36 +0300

    dynamic header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router";
+import { useUserContext } from "../../contexts/UserContext";
 export default function Header() {
+  const { isAuthenticated } = useUserContext();
   return (
     <header>
       <nav>
@@ -8,16 +10,17 @@ export default function Header() {
           <img src="./images/logo.png" alt="logo" />
         </Link>
         <Link to="/catalog">Catalog</Link>
-        {/* Logged-in users */}
-        <div id="user">
-          <Link to="/games/create">Add Game</Link>
-          <Link to="/logout">Logout</Link>
-        </div>
-        {/* Guest users */}
-        <div id="guest">
-          <Link to="/login">Login</Link>
-          <Link to="/register">Register</Link>
-        </div>
+        {isAuthenticated ? (
+          <div id="user">
+            <Link to="/games/create">Add Game</Link>
+            <Link to="/logout">Logout</Link>
+          </div>
+        ) : (
+          <div id="guest">
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </div>
+        )}
       </nav>
     </header>
   );
